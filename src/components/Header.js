f57873f00d85db914660f6fc34f91bcf5885a3f4
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-03-16 03:27:42 +0300

    added css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, {  useState } from 'react'
 import { useDispatch } from "react-redux";
 import {number} from "../redux/countSlice"
-
+import "../style/header.css"
 const Header = () => {
 
   
