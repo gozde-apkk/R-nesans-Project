18a772016f51cd980b3a586126a06641e123191f
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-03-16 03:20:36 +0300

    created singleuser component

---
+++ src/components/SingleUser.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SingleUser = ({ user }) => {
+  return (
+    <div className="single">
+      <div>
+        <img src={user?.picture.large} alt="profile"/>
+      </div>
+      <div className="info">
+        {" "}
+        <p>{user?.name.first} {user?.name.last} </p>
+        <p> Email : {user?.email} </p>
+        <p> {user?.location.city} | {user?.location.country} </p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUser;
